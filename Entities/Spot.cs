6fae7317b480ef89b8cafffb9dc8589d8710c6d1
teofurlan <teofurlan03@gmail.com>
2023-09-24 16:47:05 -0300

    el diablo

--- Entities/Spot.cs
+++ Entities/Spot.cs
@@ -3,20 +3,19 @@ namespace Parking.Entities;
 public class Spot
 {
     public int Id { get; set; }
-    public string PositionX { get; set; }
-    public int PositionY { get; set; }
+    public char Row { get; set; }
+    public int Colunm { get; set; }
+    public int LotId { get; set; }
     public bool Occupied { get; set; }
 
-    public Spot()
-    {
-        
-    }
+    public Spot(){}
 
-    public Spot(int Id, string PositionX, int PositionY)
+    public Spot(int Id, char Row, int Column, int LotId)
     {
         this.Id = Id;
-        this.PositionX = PositionX;
-        this.PositionY = PositionY;
+        this.Row = Row;
+        this.Colunm = Column;
+        this.LotId = LotId;
         Occupied = false;
     }
 }
