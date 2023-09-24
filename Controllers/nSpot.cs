96c81581b02b0fa4a59785849eabd528836d0d94
teofurlan <teofurlan03@gmail.com>
2023-09-24 16:35:40 -0300

    xd

--- Controllers/nSpot.cs
+++ Controllers/nSpot.cs
@@ -1,76 +1,89 @@
+using System.Data;
 using Parking.Entities;
 
 namespace Parking.Controllers;
 
 public class nSpot
 {
-    public static void Add(int i)
+    public static Spot Create(char Row, int Column, int LotId)
     {
-        int index = i;
-        foreach( Lot lot in Program.lots){
-            if(lot.Id == index){
-                nLot.List(true);
-                Console.WriteLine("Ingrese el ID del estacionamiento: ");
-                int Id = Tools.ValidateInt();
-                Console.WriteLine("Ingrese la zona: ");
-                string Zone = Console.ReadLine();
-                Console.ReadLine();
-                Console.WriteLine("Ingrese la posición en Y: ");
-                int PosY = Tools.ValidateInt();
-                lot.spots.Add(new Spot(Id, Zone, PosY));
-            }
-        }
+        int Id = Program.spots.Count;
+        Spot spot = new Spot(Id, Row, Column, LotId);
+        Program.spots.Add(spot);
+        return spot;    
     }
-    public static void List(int i)
+    public static int SelectByPosition()
     {
-        int index = i;
-        foreach( Lot lot in Program.lots){
-            if(lot.Id == index){
-                nLot.List(true);
-            }
-        }
-    }
-
-    public static void Erase()
-    {
-
-
-    }
-
-    public static void Modify(int i)
-    {
-
-    }
-
-    public static int Select()
-    {
-        Console.WriteLine();
+        
         return 0;
     }
+    // public static void Add(int i)
+    // {
+    //     int index = i;
+    //     foreach( Lot lot in Program.lots){
+    //         if(lot.Id == index){
+    //             nLot.List(true);
+    //             Console.WriteLine("Ingrese el ID del estacionamiento: ");
+    //             int Id = Tools.ValidateInt();
+    //             Console.WriteLine("Ingrese la zona: ");
+    //             string Zone = Console.ReadLine();
+    //             Console.ReadLine();
+    //             Console.WriteLine("Ingrese la posición en Y: ");
+    //             int PosY = Tools.ValidateInt();
+    //             lot.spots.Add(new Spot(Id, Zone, PosY));
+    //         }
+    //     }
+    // }
+    // public static void List(int i)
+    // {
+    //     int index = i;
+    //     foreach( Lot lot in Program.lots){
+    //         if(lot.Id == index){
+    //             nLot.List(true);
+    //         }
+    //     }
+    // }
 
-    public static void Menu()
-    {
-        string[] opciones = new string[] { "Add", "Modify", "Erase", "List" };
-        int seleccion = Tools.Menu("Spot Menu", opciones);
-        switch (seleccion)
-        {
-            case 1:
-                Add(nLot.Select());
-                Menu();
-                break;
-            case 2:
-                Modify(Select());
-                Menu();
-                break;
-            case 3:
-                Erase();
-                Menu();
-                break;
-            case 4:
-                List(nLot.Select()); Console.ReadKey();
-                Menu();
-                break;
-            case 0: break;
-        }
-    }
+    // public static void Erase()
+    // {
+
+
+    // }
+
+    // public static void Modify(int i)
+    // {
+
+    // }
+
+    // public static int Select()
+    // {
+    //     Console.WriteLine();
+    //     return 0;
+    // }
+
+    // public static void Menu()
+    // {
+    //     string[] opciones = new string[] { "Add", "Modify", "Erase", "List" };
+    //     int seleccion = Tools.Menu("Spot Menu", opciones);
+    //     switch (seleccion)
+    //     {
+    //         case 1:
+    //             Add(nLot.Select());
+    //             Menu();
+    //             break;
+    //         case 2:
+    //             Modify(Select());
+    //             Menu();
+    //             break;
+    //         case 3:
+    //             Erase();
+    //             Menu();
+    //             break;
+    //         case 4:
+    //             List(nLot.Select()); Console.ReadKey();
+    //             Menu();
+    //             break;
+    //         case 0: break;
+    //     }
+    // }
 }
