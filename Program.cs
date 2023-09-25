824adb7e08056e5f17ac73008527c4bc766a73eb
DylanRoth5 <130003124+DylanRoth5@users.noreply.github.com>
2023-09-25 16:46:41 -0300

    Fix

--- Program.cs
+++ Program.cs
@@ -18,10 +18,11 @@ namespace Parking
             vehicles = new List<Vehicle>();
             List<string> lotes = Tools.FileGetType("Lot","Data.txt");
             foreach(string lote in lotes){
-                Console.WriteLine(lote);
+                string[] data = lote.Split(',');
             }
-            Console.ReadKey();
+            Tools.HaltProgramExecution();
             Menu();
+            Save();
         }
         public static void Menu()
         {
@@ -31,23 +32,24 @@ namespace Parking
                 case 1: nLot.Menu(); Menu(); break;
                 case 2: nTicket.Menu(); Menu(); break;
                 case 3: nVehicle.Menu(); Menu(); break;
-                case 4: Save(); Menu(); break;
                 case 0: break;
             }
         }
         public static void Save(){
-            foreach (Lot lot in lots){
+            Console.WriteLine("Saving Changes");
+            if(lots != null){ foreach (Lot lot in lots){
                 Tools.FileWrite("Lot",$"{lot.Id},{lot.Name},{lot.Address},{lot.HourPrice}","Data.txt");
-            }
-            foreach (Spot spot in spots){
+            }}else{Console.WriteLine("There are no lots");}
+            if(spots != null){ foreach (Spot spot in spots){
                 Tools.FileWrite("Spot",$"{spot.Id},{spot.Row},{spot.Column},{spot.Occupied},{spot.LotId}","Data.txt");
-            }
-            foreach (Ticket ticket in tickets){
+            }}else{Console.WriteLine("There are no spots");}
+            if(tickets != null){ foreach (Ticket ticket in tickets){
                 Tools.FileWrite("Ticket",$"{ticket.Id},{ticket.Total},{ticket.Entry},{ticket.Exit},{ticket.Spot.Id}.{ticket.Vehicle.Id}","Data.txt");
-            }
-            foreach (Vehicle vehicle in vehicles){
+            }}else{Console.WriteLine("There are no tickets");}
+            if(vehicles != null){ foreach (Vehicle vehicle in vehicles){
                 Tools.FileWrite("Vehicle",$"{vehicle.Id},{vehicle.Brand},{vehicle.Model},{vehicle.Plate}","Data.txt");
-            }
+            }}else{Console.WriteLine("There are no vehicles");}
+            Tools.HaltProgramExecution();
         }
     }
 }
