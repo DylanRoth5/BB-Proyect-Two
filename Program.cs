703a5499d144200a26b9bb7efacb1d5673de2063
Enzo <enzolabatte@gmail.com>
2023-10-01 18:10:32 -0300

    data test

--- Program.cs
+++ Program.cs
@@ -34,10 +34,18 @@ namespace Parking
         }
         public static void Data()
         {
+            DateTime dt1 = new DateTime(2023, 10, 3, 7, 10, 24);
+            DateTime dt2 = new DateTime(2023, 10, 3, 9, 23, 36);
+
             nVehicle.Create("Mustang", "Ford", "11-AAAA-11");
             nVehicle.Create("Miata", "Mazda", "22-BBBB-22");
-            nVehicle.Create("Corsa", "Chevrolet", "33-BBBB-33");
+            nVehicle.Create("Corsa", "Chevrolet", "33-CCCC-33");
+
             nLot.Create("Playa Damián", "Rosario del Tala", 1150.50m, 3, 7);
+            nLot.Create("Playa Matias", "Valle Maria", 990.25m, 4, 8);
+            nLot.Create("Playa Navy", "Posadas", 1300.75m, 6, 10);
+
+            nTicket.Create(1,dt1, dt2, 1, 1);
         }
         public static void Load(){
             
